5669f01505408be5cff9090504c2f7ee1b5acd93
gretchenmarie <gretchenmward@gmail.com>
2018-07-17 13:12:24 -0500

    adding files

---
+++ main.js
@@ -0,0 +1,68 @@
+const TurnThePageBooks = Object.create({}, {
+    business: {
+        value: "Turn-the-Page"
+
+    },
+    employmentStart: {
+        value: "03-01-2002"
+    },
+    employmentEnd: {
+        value: "01-01-2004"
+    },
+    liked: {
+        value: "I was my own boss."
+    },
+    disliked: {
+        value: " doing my taxes a business owner"
+    },
+    hoursPerWeek:{
+        value: " 60 hours per week"
+    }
+
+})
+
+const Dreamcrafters = Object.create({}, {
+    business: {
+        value: "DreamCrafters"
+    },
+    employmentStart: {
+        value: "01-01-2000"
+    },
+    employmentEnd: {
+        value: "10-01-2016"
+    },
+    liked: {
+        value: "worked from home"
+    },
+    disliked: {
+        value: " unreliable hours"    
+    },
+    hoursPerWeek: {
+        value: " 20"
+    }
+})
+
+const StTomasHealthAtHOme = Object.create({}, {
+    business: {
+        value: "St-Thomas-Health-At-Home"
+    },
+    employmentStart: {
+        value: "12-05-2017"
+    },
+    employmentEnd: {
+        value: "04-28-2018"
+    },
+    liked: {
+        value: "very nice environment"
+    },
+    disliked: {
+        value: "early morning"    
+    },
+    hoursPerWeek: {
+        value: "40"
+    }
+})
+
+const JobMaker = (a,r,g,u,m,e,n,t,s) {
+    return theObjectYouBuilt
+}
---
+++ readme.md
@@ -0,0 +1,31 @@
+Practice
+Use Object.create() to define objects for every place that you've worked before NSS.
+
+For example:
+
+const ChickFilA = Object.create({}, {
+    business: {
+        value: "Chick-Fil-A"
+    },
+    employmentStart: {
+        value: "01-01-2009"
+    },
+    employmentEnd: {
+        value: "01-01-2010"
+    }
+})
+You add as many properties as you want for each job.
+
+Things you liked about the job
+Things you hated about the job
+etc...
+
+Challenge
+Write a function that will build, and return, a job object. Make sure you define an argument for each one of the properties you want to be in the object.
+
+const JobMaker = (a,r,g,u,m,e,n,t,s) {
+    return theObjectYouBuilt
+}
+Advanced Challenge
+Put each of your jobs into an array.
+Iterate over the array, and use document.createElement() to build an <article> element representing each job. The id property of the element should be the name of the business.# job-history
